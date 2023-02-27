95a4e9f47f2b4bc24283f6d072bdc5f71d1aa577
JoaoNS <joaopnunes252@gmail.com>
2023-02-27 14:49:45 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/joaonun/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaonun/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.joaonun.cursomc.services;
 
 import java.util.Optional;
 
+import com.joaonun.cursomc.services.excpetions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria Buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
