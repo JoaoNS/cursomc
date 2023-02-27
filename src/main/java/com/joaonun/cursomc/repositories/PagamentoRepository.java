b550e9d4a9906fd39d68285e23c637653537d8d5
JoaoNS <joaopnunes252@gmail.com>
2023-02-27 19:04:54 -0300

    Pedido, estadoPagamento e Pagamento

---
+++ src/main/java/com/joaonun/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.joaonun.cursomc.repositories;
+
+import com.joaonun.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
---
+++ src/main/java/com/joaonun/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.joaonun.cursomc.repositories;
+
+import com.joaonun.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
